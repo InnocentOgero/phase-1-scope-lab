86ac754d4f2562f9519d106dfdf92507e9ccaf7e
InnocentOgero <innocent.ogero@student.moringaschool.com>
2023-09-25 10:45:44 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Write your solution in this file!
+// Write your solution in this file!var customerName='bob';
+var customerName='bob';
+const leastFavoriteCustomer="Victor Anekesa";
+function upperCaseCustomerName(){
+    customerName=customerName.toUpperCase()
+    return customerName
+}
+function setBestCustomer(){
+    bestCustomer='not bob';
+    return bestCustomer
+}
+function overwriteBestCustomer(){
+    bestCustomer="maybe bob";
+    return bestCustomer
+}
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer='Reagan Munene'
+    return leastFavoriteCustomer
+
+
+}
+
+
